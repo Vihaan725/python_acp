61cb3ff3adeccf1677e1da09e157c89b687defc7
Vihaan725 <vihaanveks725@gmail.com>
2026-04-30 19:02:43 -0400

    Add files via upload

---
+++ age_check.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your age: "))
+if 10<=a<=20:
+    print("You are eligible!")
+else:
+    print("You are not eligible!")
---
+++ power_calculator.py
@@ -0,0 +1,3 @@
+a = int(input("Enter a power: "))
+b = int(input("Enter a base: "))
+print(b**a)
